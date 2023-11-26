20424e4362ac214b410c893a58807d745f07474b
Matheus Jacob <70240646+Matheus-jacobb@users.noreply.github.com>
2023-11-26 09:45:50 -0300

    refactor(index.py): removed unecessary function

--- index.py
+++ index.py
@@ -23,6 +23,15 @@ classificador_emocoes = load_model(caminho_modelo, compile=False)
 def detectar_emocoes(imagem):
     original = imagem.copy()
     cinza = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    
+    # @param cinza: A imagem em escala de cinza na qual você deseja realizar a detecção de faces.
+    # @param scaleFactor: Este parâmetro compensa a redução da taxa de escala entre os tamanhos da 
+    #                     imagem. Ele é usado para criar uma pirâmide de imagens em diferentes escalas 
+    #                     para detecção de objetos em várias escalas. Um valor mais baixo tornará a detecção mais sensível, mas também mais lenta.
+    # @param minNeighbors: Número de vizinhos que cada candidato a retângulo de face deve ter para ser considerado parte de uma face. 
+    #                      Este parâmetro ajuda a filtrar falsos positivos. Valores mais altos resultam em uma detecção mais conservadora.
+    # @param minSize: Tamanho mínimo da face possível. Qualquer retângulo menor que isso é ignorado. 
+    #                 Isso ajuda a remover detecções insignificantes e a melhorar a precisão.
     faces = face_detection.detectMultiScale(cinza, scaleFactor=1.1, minNeighbors=3, minSize=(20, 20))
 
     if len(faces) > 0:
